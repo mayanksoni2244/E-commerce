c5ebd116f9dccae8012b2010a7b9a4ea27490be8
mayank soni <ms9218764@gmail.com>
2025-07-03 20:18:18 +0530

    Update login.jsx

--- Frontend/src/login.jsx
+++ Frontend/src/login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
   async function handleSubmit(e) {
     e.preventDefault();
     try {
-      await axios.post("https://fabrico-o87m.onrender.com", formData);
+      await axios.post("https://fabbackend.onrender.com", formData);
       toast.success("Logged in Successfully");
       login(formData);
       navigate("/home");
